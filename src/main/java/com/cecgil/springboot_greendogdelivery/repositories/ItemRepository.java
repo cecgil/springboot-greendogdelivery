a68b06f0e2278ee76baa95e1f49161624a4461ac
cecgil <dudigil6@gmail.com>
2025-05-09 01:32:41 -0300

    Ate pag 92

--- src/main/java/com/cecgil/springboot_greendogdelivery/repositories/ItemRepository.java
+++ src/main/java/com/cecgil/springboot_greendogdelivery/repositories/ItemRepository.java
@@ -1,9 +1,11 @@
 package com.cecgil.springboot_greendogdelivery.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.cecgil.springboot_greendogdelivery.entities.ItemOrder;
 
+@RepositoryRestResource(collectionResourceRel="itens",path="itens")
 public interface ItemRepository extends JpaRepository<ItemOrder, Long> {
     
 }
